c261b663230dc75c89170e6bcb60c02e37634254
Laiz Lira <laizgamalira@gmail.com>
2021-08-17 10:47:14 -0300

    complete challenge

---
+++ src/components/Content.js
@@ -0,0 +1,52 @@
+import styles from '../styles/components/Content.module.scss'
+import { BsClock } from 'react-icons/bs';
+
+export function Content() {
+    return(
+        <div className={styles.contentContainer}>
+            <div className={styles.header}>
+                <p>Está disponível agora!</p>
+                <h1>Loki</h1>
+            </div>
+
+            <section className={styles.infoContainer}>
+                <div className={styles.cover}>
+                    <picture>
+                        <img src="capa.png" alt="Loki" />
+                    </picture>
+                </div>
+                <div className={styles.info}>
+                    <div className={styles.synopsis}>
+                        <p>
+                            Em Loki da Marvel Studios, o vilão mercurial Loki retoma
+                            seu papel como o Deus do Mal em uma nova série que ocorre
+                            após os eventos de Vingadores: Endgame.
+                        </p>
+                    </div>
+
+                    <div className={styles.techInfo}>
+                        <span className={styles.duration}><BsClock />51min</span>
+                        <div className={styles.starsRating}>
+                            <input type="radio" id="st1" value="1" />
+                            <label htmlFor="st1"></label>
+                            <input type="radio" id="st2" value="2" />
+                            <label htmlFor="st2"></label>
+                            <input type="radio" id="st3" value="3" />
+                            <label htmlFor="st3"></label>
+                            <input type="radio" id="st4" value="4" />
+                            <label htmlFor="st4"></label>
+                            <input type="radio" id="st5" value="5" />
+                            <label htmlFor="st5"></label>
+                        </div>
+                        <span>2021</span>
+                    </div>
+
+                    <div className={styles.actionButtons}>
+                        <button className={`${styles.watchNowButton} btn`}>Assistir agora</button>
+                        <button className={`${styles.trailerButton} btn`}>Trailer</button>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { Content } from '../components/Content'
 import styles from '../styles/pages/index.module.scss'
 
 export default function Home() {
@@ -8,14 +9,14 @@ export default function Home() {
         <title>Codelândia - Desafio 5</title>
         <meta name="description" content="Praticando Front-end com a Codelândia" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Merriweather:wght@400;700&display=swap" rel="stylesheet" />
 
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=B612:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </Head>
 
       <main className={styles.main}>
-        
+        <Content />
       </main>
 
     </div>
